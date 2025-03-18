12d49907b6febe6f0595e8fe6d91b5694926d8cc
Naitik khoriya <naitikkhoriya144263@gmail.com>
2025-03-18 13:37:04 +0530

    Add loader animation with timer and font updates

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-gsap.from(".line h1", {
+var tl = gsap.timeline();
+tl.from(".line h1", {
   y: 150,
   //   opacity: 0,
   stagger: 0.25,
   duration: 1,
 });
-  
+
+tl.from("#line1-part1", {
+  opacity: 0,
+  onStart: function () {
+    var h5timer = document.querySelector("#line1-part1 h5");
+    var grow = 0;
+    setInterval(function () {
+      if (grow < 100) {
+        h5timer.innerHTML = grow++;
+      } else {
+        h5timer.innerHTML = grow;
+      }
+    }, 30);
+  },
+});
+tl.to(" .line h2", {
+  AnimationName: anime,
+  opacity: 1,
+
+  //   stagger: 0.25,
+});
+tl.to("#loader", {
+  opacity: 0,
+  duration: 0.4,
+  delay: 3.5,
+});
+
+tl.from("#page1", {
+  y: 1200,
+  opacity: 0,
+  delay: 0.2,
+});
+tl.to("#loader", {
+  display: "none",
+  delay: 0.2,
+});
